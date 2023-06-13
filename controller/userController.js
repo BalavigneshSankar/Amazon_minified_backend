84a1a2b487d90008fdda0139e18bcf45c95032df
BalavigneshSankar <balavignesh.shankar@gmail.com>
2023-06-13 16:10:03 +0530

    Changed path verb for getting usename

---
+++ controller/userController.js
@@ -0,0 +1,13 @@
+const catchAsync = require("../utils/catchAsync");
+const User = require("./../models/userModel");
+
+exports.getUser = catchAsync(async (req, res, next) => {
+  const user = await User.findById(req.params.id).select("name");
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      user,
+    },
+  });
+});
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const authController = require("./../controller/authController");
+const userController = require("./../controller/userController");
 
 const router = express.Router();
 
+router.route("/:id").get(authController.protect, userController.getUser);
 router.route("/signup").post(authController.signUp);
 router.route("/logIn").post(authController.logIn);
 
